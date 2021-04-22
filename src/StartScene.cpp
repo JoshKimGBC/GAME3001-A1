76ff5d8cd319336ed8ed6f69db816e593e27ceec
Joshua Kim <joshua.kim@georgebrown.ca>
2021-04-21 22:49:53 -0400

    i deleted this by accident and had to remake it....

---
+++ src/StartScene.cpp
@@ -0,0 +1,73 @@
+#include "StartScene.h"
+#include <algorithm>
+#include "Game.h"
+#include "glm/gtx/string_cast.hpp"
+#include "EventManager.h"
+#include "Renderer.h"
+
+StartScene::StartScene() {
+	StartScene::start();
+}
+
+StartScene::~StartScene()
+= default;
+
+void StartScene::draw() {
+
+	SDL_SetRenderDrawColor(Renderer::Instance()->getRenderer(), 68, 68, 192, 255);
+	drawDisplayList();
+}
+
+void StartScene::update() {
+	updateDisplayList();
+}
+
+void StartScene::clean() {
+	removeAllChildren();
+}
+
+void StartScene::handleEvents() {
+	EventManager::Instance().update();
+
+	// Keyboard Events
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE)) {
+		TheGame::Instance()->quit();
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1)) {
+		TheGame::Instance()->changeSceneState(PLAY_SCENE);
+	}
+}
+
+void StartScene::start() {
+	const SDL_Color blue = { 0, 0, 255, 255 };
+	m_pTitleLabel = new Label("Assignment 1", "Consolas", 54, blue, glm::vec2(400.0f, 140.0f));
+	m_pTitleLabel->setParent(this);
+	addChild(m_pTitleLabel);
+
+	m_pNameLabel = new Label("Joshua Kim - 101298061", "Consolas", 32, blue, glm::vec2(400.0f, 220.0f));
+	m_pNameLabel->setParent(this);
+	addChild(m_pNameLabel);
+
+	// Start Button
+	m_pStartButton = new Button();
+	m_pStartButton->getTransform()->position = glm::vec2(400.0f, 400.0f);
+
+	m_pStartButton->addEventListener(CLICK, [&]()-> void {
+		m_pStartButton->setActive(false);
+		TheGame::Instance()->changeSceneState(PLAY_SCENE);
+		});
+
+	m_pStartButton->addEventListener(MOUSE_OVER, [&]()->void {
+		m_pStartButton->setAlpha(128);
+		});
+
+	m_pStartButton->addEventListener(MOUSE_OUT, [&]()->void {
+		m_pStartButton->setAlpha(255);
+		});
+
+	addChild(m_pStartButton);
+
+
+}
+
---
+++ src/StartScene.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef __START_SCENE__
+#define __START_SCENE__
+
+#include "Scene.h"
+#include "Label.h"
+#include "ship.h"
+#include "Button.h"
+
+class StartScene final : public Scene {
+
+private:
+	Label* m_pTitleLabel{ };
+	Label* m_pNameLabel{ };
+
+	Button* m_pStartButton;
+
+public:
+	StartScene();
+	~StartScene();
+
+	// Inherited via Scene
+	virtual void draw() override;
+	virtual void update() override;
+	virtual void clean() override;
+	virtual void handleEvents() override;
+	virtual void start() override;
+
+};
+
+#endif /* defined (__START_SCENE__) */
